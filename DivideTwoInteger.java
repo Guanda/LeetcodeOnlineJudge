c9c6bcac8f329addb393a2ea6a85657d78eed344
Guanda <guanda.wang@gmail.com>
2015-04-26 17:05:02 -0400

    Update the divide two integer problem, make it pass all the test case

--- DivideTwoInteger.java
+++ DivideTwoInteger.java
@@ -3,20 +3,28 @@ Divide two integers without using multiplication, division and mod operator.
 
 If it is overflow, return MAX_INT.
 
+Analysis:
+	Shift can be used to solve this problem. We shift the divisor left until it 
+	just smaller than dividend but if we keep shifting one more bit, it’s larger 
+	than dividend. Than we can add the shifted value to the result and subtract 
+	the shifted value from dividend. Keep doing this until dividend is smaller 
+	than divisor. In fact, every integer can be represented by a set of base 2 so 
+	that shifting can be used.
+
 */
 
 public class DivideTwoInteger
 {
 	public int divideTwoInteger(int dividend, int divisor)
 	{
-		long p = Math.abs(dividend);
-		long q = Math.abs(divisor);
-		int result = 0
+		long p = Math.abs((long)dividend);
+		long q = Math.abs((long)divisor);
+		int result = 0;
 
 		while(p >= q)
 		{
 			int counter = 0;
-			while(p >= (q << 1))
+			while(p >= (q << counter))
 			{
 				counter++;
 			}
@@ -24,6 +32,11 @@ public class DivideTwoInteger
 			p -= q << (counter - 1);
 		}
 
+		//take care of the boundry case
+		if(dividend == Integer.MIN_VALUE && divisor == -1)
+		{
+			return Integer.MAX_VALUE;
+		}
 		if((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0))
 		{
 			return result;
