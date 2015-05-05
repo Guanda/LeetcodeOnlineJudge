e8acba9f01090d6dc0ffdd6742b616f381e5d865
Guanda <guanda.wang@gmail.com>
2015-05-04 21:32:55 -0400

    Reverse linked list, two methods, iteration and recursive

---
+++ ReverseLinkedList.java
@@ -0,0 +1,54 @@
+/*
+Reverse a singly linked list.
+
+Analysis:
+Method 1: Just one iteration. Move current to connect previous one. So we need prev, curr, next these three nodes.
+Method 2: recursive solution
+
+*/
+
+class ReverseLinkedList
+{
+	//Method 1
+	public ListNode reverseList(ListNode head)
+	{
+		if(head == null || head.next == null)
+			return head;
+
+		ListNode prev = null;
+		ListNode curr = head;
+		ListNode next = null;
+
+		while(curr != null)
+		{
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		return prev;
+	}
+
+
+	//Method 2
+	public ListNode reverseList(ListNode head)
+	{
+		if(head == null || head.next == null)
+			return head;
+
+		ListNode remainingReverse = reverseList(head.next);
+
+		//update the tail as beginning
+		ListNode curr = remainingReverse;
+		while(curr.next != null)
+		{
+			curr = curr.next;
+		}
+
+		//assign the head as tail
+		curr.next = head;
+		head.next = null;
+
+		return remainingReverse;
+	}
+}
