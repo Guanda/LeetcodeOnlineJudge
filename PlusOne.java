0bed2bddd3eaab7f7967cba3ae518e5b4d8f46d3
Guanda <guanda.wang@gmail.com>
2015-05-13 20:45:49 -0400

    Plus one for an integer which represented as an array of digits

---
+++ PlusOne.java
@@ -0,0 +1,34 @@
+/*
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Analysis:
+In case of very large number, so we cannot use revert to integer to calculate.
+Classic add-with-carry problem.
+
+*/
+
+class PlusOne
+{
+	public int[] plusOne(int[] digits)
+	{
+		int carry = 1;
+		int[] result = new int[digits.length];
+		for(int i = digits.length - 1; i >= 0; i--)
+		{
+			int val = digits[i] + carry;
+			result[i] = val % 10;
+			carry = val / 10;
+		}
+
+		//consider the first digit for the result
+		if(carry == 1)
+		{
+			result = new int[digits.length + 1];
+			result[0] = 1;
+		}
+		
+		return result;
+	}
+}
