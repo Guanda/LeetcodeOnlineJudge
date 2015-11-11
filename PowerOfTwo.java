378c5dd98a1a401edd8912191406dbb8fe9926be
Guanda Wang <guanda.wang@gmail.com>
2015-11-11 11:49:23 -0500

    Check if a number is power of two

---
+++ PowerOfTwo.java
@@ -0,0 +1,17 @@
+/*
+Given an integer, write a function to determine if it is a power of two.
+
+*/
+
+public class PowerOfTwo
+{
+	public boolean isPowerOfTwo(int n)
+	{
+		while(n % 2 == 0 && n > 1)
+		{
+			n = n / 2;
+		}
+		
+		return (n == 1);
+	}
+}
