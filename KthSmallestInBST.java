281c2a673ec53aed9675723dfaea3f35afc86aaf
Guanda Wang <guanda.wang@gmail.com>
2015-11-29 05:21:13 -0500

    two more method for find the kth smallest element in BST

--- KthSmallestInBST.java
+++ KthSmallestInBST.java
@@ -14,6 +14,9 @@ Try to utilize the property of a BST.
 What if you could modify the BST node's structure?
 The optimal runtime complexity is O(height of BST).
 
+Analysis:
+	3 methods here.
+
 */
 
 /**
@@ -27,7 +30,7 @@ The optimal runtime complexity is O(height of BST).
  */
 public class KthSmallestInBST
 {
-	//inorder traverse, time: O(n)
+	//inorder traverse, time: O(n), space O(n)
 	public int kthSmallest(TreeNode root, int k)
 	{
 		int result = 0;
@@ -50,5 +53,76 @@ public class KthSmallestInBST
 			}
 		}
 		return result;
+	}
+
+
+	//get size of BST and recursively find out kth element
+	//time: O(nlogn), space: O(1). size BST is time O(n)
+	public int kthSmallest2(TreeNode root, int k)
+	{
+		int leftNodeSize = size(root.left);
+		if(k == leftNodeSize + 1) {
+			return root.val;
+		}
+		else if(k < leftNodeSize + 1) {
+			return kthSmallest2(root.left, k);
+		}
+		else {
+			return kthSmallest2(root.right, k - leftNodeSize - 1);
+		}
+	}
+
+	private int size(TreeNode root)
+	{
+		if(root == null)
+			return 0;
+
+		return size(root.left) + size(root.right) + 1;
+	}
+
+
+	//if we can modify the tree node's structure, add size N to it
+	//the search only takes O(logN)
+	public class TreeNode {
+	    int val;
+	    //number of nodes in subtree (including itself)
+	    int n;  
+	    TreeNode left, right;
+	    TreeNode(int x) { 
+	    	val = x; 
+	    }
+
+	    public int size(TreeNode root)
+		{
+			if(root == null)
+				return 0;
+
+			return root.n;
+		}
+
+		public void insert(int val)
+		{
+			p = insert(p, val);
+		}
+
+		private TreeNode insert(TreeNode p, int val)
+		{
+			if(p == null) {
+				return new TreeNode(val);
+			}
+			if(val < p.val) {
+				p.left = insert(p.left, val);
+			}
+			else if(val > p.val) {
+				p.right = insert(p.right, val);
+			}
+			else {
+				p.val = val;
+			}
+
+			p.n = 1 + size(p.left) + size(p.right);
+
+			return p;
+		}
 	}
 }
