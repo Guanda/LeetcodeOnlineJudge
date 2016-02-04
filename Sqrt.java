1838aa80d8340b13a658fbe7722496636e52905b
Guanda Wang <guanda.wang@gmail.com>
2016-02-03 22:08:38 -0500

    Add binary search solution for sqrt()

--- Sqrt.java
+++ Sqrt.java
@@ -2,11 +2,13 @@
 Implement int sqrt(int x).
 
 Analysis:
-	Use Newton Method
+	1. Use Newton Method
+	2. Use binary search
 
 */
 
 class Sqrt {
+	//newton method
 	public int sqrt(int n) {
 		if(n == 0)
 			return 0;
@@ -17,5 +19,23 @@ class Sqrt {
 			res = (res + n / res) / 2;
 		}
 		return (int)res;
+	}
+
+	//binary search
+	public int sqrt2(int n) {
+		long i = 0;
+		long j = n / 2 + 1;
+		while(i <= j) {
+			long mid = (i + j) / 2;
+			if(mid * mid == n)
+				return (int)mid;
+			else if(mid * mid < n) {
+				i = mid + 1;
+			}
+			else {
+				j = mid - 1;
+			}
+		}
+		return (int)j;
 	}
 }
