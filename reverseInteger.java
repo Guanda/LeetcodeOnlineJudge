362ecb92ef02fa1966c79a54a7e6fbc448a1b13e
Guanda Wang <guanda.wang@gmail.com>
2016-02-05 18:26:12 -0500

    Update indent

--- reverseInteger.java
+++ reverseInteger.java
@@ -20,12 +20,12 @@ Throw an exception? Good, but what if throwing an exception is not an option? Yo
 public class ReverseInteger {
     public int reverse(int x) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
-    int r = 0;
+	    int r = 0;
 
-    while(x != 0) {
-        r = r*10 + x % 10;
-        x /= 10;
-    }
-    return r;
+	    while(x != 0) {
+	        r = r*10 + x % 10;
+	        x /= 10;
+	    }
+	    return r;
     }
 }
