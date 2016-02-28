355ed9c2a501a28b52527ab2b8244a80807b4bc1
Guanda Wang <guanda.wang@gmail.com>
2016-02-27 22:18:45 -0500

    String to integer problem update

--- stringToInteger.java
+++ stringToInteger.java
@@ -2,43 +2,62 @@
  Implement atoi to convert a string to an integer.  
  */
 
-public class StringToInteger {
-	public int atoi(String str) {
-		if (str == null || str.length() == 0)
-			return 0;
-		long num = 0;
-		int j = 0;
-		str = str.trim();
-		if (str.charAt(0) == '+' || str.charAt(0) == '-') {
-			j++;
-		}
-		boolean isNeg = false;
-		if (str.charAt(0) == '-') {
-			isNeg = true;
-		}
-
-		for (int i = j; i < str.length(); i++) {
-			if (((int) str.charAt(i) >= 48) && ((int) str.charAt(i) <= 57)) {
-				num = num * 10 + ((int) str.charAt(i) - 48);
-				if (num > Integer.MAX_VALUE) {
-					if (isNeg == false)
-						return Integer.MAX_VALUE;
-					else
-						return Integer.MIN_VALUE;
-				} else if (num == Integer.MAX_VALUE) {
-					if (isNeg == false)
-						return Integer.MAX_VALUE;
-					else
-						return Integer.MIN_VALUE + 1;
-				}
-			} else {
-				break;
-			}
-		}
-
-		if (isNeg)
-			return (int) num * (-1);
-		else
-			return (int) num;
-	}
+/*
+ * The method name below is parseLong, which is not a good name for covert string to "Integer".
+ * I should change it to parseInteger but I cannot compile it based on that name, so I leave it there. 
+ * And the same as return type, change from long to int.
+ *
+ * Here I have some limitations for this problem:
+ * 1. When input string is null or empty string, I will return null.
+ * 2. I will use trim method to ignore all the spaces in the string.
+ * 3. If the number is larger than max integer or smaller than min integer, throw exception.
+ * 4. If there is any character is not between 0 to 9 except the first character are + or -, throw exception.
+ *
+ *
+ */
+static int parseLong(String str) {
+    if(str == null || str.length() == 0)
+        throw new NumberFormatException("Not a valid input");
+    
+    //make num long type since the number may overflow or underflow for integer.
+    long num = 0;
+    int start = 0;
+    
+    //ignore all spaces in input string
+    str = str.trim();
+    
+    //handle the first char
+    if(str.charAt(0) == '+' || str.charAt(0) == '-')
+        start++;
+    
+    //handle the first char see if the integer will be positive or negative.
+    boolean isNeg = false;
+    if(str.charAt(0) == '-')
+        isNeg = true;
+    
+    for(int i = start; i < str.length(); i++) {
+        //check if the current character is between 0(ascii 48) to 9(ascii 57).
+        if((int)str.charAt(i) >= 48 && (int)str.charAt(i) <= 57) {
+            num = num * 10 + ((int)str.charAt(i) - 48);
+            if(num > Integer.MAX_VALUE) {
+                if(!isNeg)
+                    throw new ArithmeticException("overflow occurred!");
+                else if(num > (long) Integer.MAX_VALUE + 1)
+                    throw new ArithmeticException("underflow occurred");
+                else
+                	break;
+            }
+        } else {
+            throw new NumberFormatException("Not a valid integer");
+        }
+    }
+    
+    if(isNeg)
+        return (int)num * (-1);
+    else
+        return (int)num;
 }
+
+
+
+
