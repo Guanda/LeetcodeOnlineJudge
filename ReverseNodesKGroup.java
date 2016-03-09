5b175de24c4ab04ed9ac3cc2e1b0e1566fc65b1c
Guanda Wang <guanda.wang@gmail.com>
2016-03-08 23:51:23 -0500

    Reverse linked list in K group

---
+++ ReverseNodesKGroup.java
@@ -0,0 +1,71 @@
+/*
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class ReverseNodesKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k <= 1) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        head = dummy;
+        while (head.next != null) {
+            head = reverseNextK(head, k);
+        }
+        
+        return dummy.next;
+    }
+    
+    // reverse head->n1->..->nk->next..
+    // to head->nk->..->n1->next..
+    // return n1
+    private ListNode reverseNextK(ListNode head, int k) {
+        // check there is enought nodes to reverse
+        ListNode next = head;
+        for (int i = 0; i < k; i++) {
+            if (next.next == null) {
+                return next;
+            }
+            next = next.next;
+        }
+        
+        // reverse, in the loop, change the link direction one by one
+        ListNode n1 = head.next;
+        ListNode prev = head, curr = n1;
+        for (int i = 0; i < k; i++) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        
+        n1.next = curr;
+        head.next = prev;
+        return n1;
+    }
+}
