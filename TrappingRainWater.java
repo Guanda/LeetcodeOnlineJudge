ba36a4bb7a323493228dff041057fb6557966f5b
Guanda Wang <guanda.wang@gmail.com>
2016-03-13 21:04:24 -0400

    Optimization of trapping water problem

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -6,9 +6,20 @@ raining.
 For example, 
 Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 
+Analysis:
+
+对任意位置i，在i上的积水，由左右两边最高的bar：A[left] = max{A[j], j<i}, A[right] = max{A[j], j>i}决定。
+定义Hmin = min(A[left], A[right])，则积水量Si为：
+
+Hmin <= A[i]时，Si = 0
+Hmin > A[i]时，Si = Hmin - A[i]
+
+
+
 */
 
 class TrappingRainWater {
+	//Method 1: using left[] and right[] to maintain max of left and right
 	public int trap(int[] height) {
 		if(height == null || height.length == 0)
 			return 0;
@@ -33,6 +44,34 @@ class TrappingRainWater {
 			water += Math.min(left[i], right[i]) - height[i];
 		}
 
+		return water;
+	}
+
+	//optimize of method 1
+	public int trapBetter(int[] height) {
+		if(height == null || height.length == 0) {
+			return 0;
+		}
+
+		int water = 0;
+		int left = 0;
+		int right = 0;
+		int start = 0;
+		int end = height.length - 1;
+
+		while(start < end) {
+			left = Math.max(left, height[start]);
+			right = Math.max(right, height[end]);
+
+			if(left < right) {
+				water += left - height[start];
+				start++;
+			}
+			else {
+				water += right - height[end];
+				end--;
+			}
+		}
 		return water;
 	}
 }
