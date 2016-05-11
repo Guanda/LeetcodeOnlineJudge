87114115aeb25fb5a859dae5eafdd2ec6cc0a0a6
Guanda Wang <guanda.wang@gmail.com>
2016-05-10 22:51:47 -0400

    missing last statement for Integer Break

--- IntegerBreak.java
+++ IntegerBreak.java
@@ -45,5 +45,7 @@ class IntegerBreak {
 			return (int)Math.pow(3, n/3);
 		else if(n % 3 == 1)
 			return 2 * 2 * (int)Math.pow(3, (n - 4) / 3);
+		else
+			return 2 * (int) Math.pow(3, n/3);
 	}
 }
