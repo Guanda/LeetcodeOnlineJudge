65f410975ff0279c41fd81e7bdb4161e068e47e1
Guanda <guanda.wang@gmail.com>
2016-05-17 23:55:43 -0400

    Update ReverseWords.java

--- ReverseWords.java
+++ ReverseWords.java
@@ -19,21 +19,18 @@ class ReverseWords
 {
     public String reverseWords(String s) 
     {
-		if(s == null || s.length() == 0)
-		{
+		if(s == null || s.length() == 0){
 			return "";
 		}
 
 		//split to words by space
 		String[] arr = s.split(" ");
 		StringBuilder sb = new StringBuilder();
-		for(int i = arr.length - 1; i >= 0; i--)
-		{
-			if(!arr[i].equals(""))
-			{
+		for(int i = arr.length - 1; i >= 0; i--){
+			if(!arr[i].equals("")){
 				sb.append(arr[i]).append(" ");
 			}
 		}
-		return sb.length() == 0 ? "" : sb.substring(0, sb.length() - 1);
+		return sb.substring(0, sb.length() - 1);
     }
 }
