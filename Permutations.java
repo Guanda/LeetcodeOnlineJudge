fe141826853608e1e0a062d4a4929ebd01e27752
Guanda Wang <guanda.wang@gmail.com>
2016-08-06 13:46:01 -0400

    Add new method for permutation problem

--- Permutations.java
+++ Permutations.java
@@ -45,4 +45,27 @@ class Permutations {
 
 		return results;
 	}
+
+	//method 2, backtracing, worse time complexity than method 1
+	public List<List<Integer>> permuteBT(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		backtrack(list, new ArrayList<>(), nums);
+		return list; 
+	}
+
+	public void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
+		if(tempList.size() == nums.length) {
+			list.add(new ArrayList<>(tempList));
+		}
+		else {
+			for(int i = 0; i < nums.length; i++) {
+				//element already exists, skip
+				if(tempList.contains(nums[i]))
+					continue;
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums);
+				tempList.remove(tempList.size() - 1);
+			}
+		}
+	}
 }
