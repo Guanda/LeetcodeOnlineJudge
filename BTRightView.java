c106e6217e625f0dd11221bb606f85724b7214d0
Guanda Wang <guanda.wang@gmail.com>
2017-01-11 23:17:26 -0500

    Update the Binary Tree Right View problem, make it more clear with two different solutions

--- BTRightView.java
+++ BTRightView.java
@@ -12,43 +12,51 @@ Given the following binary tree,
 You should return [1, 3, 4].
 
 Analysis: 
-	BFS, triverse tree level by level, always return right most node of each level.
-
+	1. BFS, triverse tree level by level, always return right most node of each level.
+	2. Recursive, 
+		Each depth of the tree only select one node.
+		View depth is current size of result list.
 */
 
 class BTRightView
 {
+	//Method 1: iterate
 	public List<Integer> rightSideView(TreeNode root)
 	{
-		List<Integer> result = new List<Integer>();
-		if(root == null)
-			return result;
-
-		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
-		int count = 1;
-		int tmp = 0;
-		queue.addLast(root);
-		while(!queue.isEmpty())
-		{
-			tmp = 0;
-			for(int i = 0; i < count; i++)
-			{
-				TreeNode tmpNode = queue.removeFirst();
-				if(i == count-1)
-					result.add(tmpNode.val);
-				if(tmpNode.left != null)
-				{
-					queue.addLast(tmpNode.left);
-					tmp++;
-				}
-				if(tmpNode.right != null)
-				{
-					queue.addLast(tmpNode.right);
-					tmp++;
-				}
-			}
-			count = tmp;
-		}
-		return result;
+        List<Integer> result = new ArrayList();
+        Queue<TreeNode> queue = new LinkedList();
+        if (root == null) return result;
+        
+        queue.offer(root);
+        while (queue.size() != 0) {
+            int size = queue.size();
+            for (int i=0; i<size; i++) {
+                TreeNode cur = queue.poll();
+                if (i == 0) result.add(cur.val);
+                if (cur.right != null) queue.offer(cur.right);
+                if (cur.left != null) queue.offer(cur.left);
+            }
+            
+        }
+        return result;
 	}
+
+	//Method 2: recursive
+	public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+    
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+        
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+    }
 }
