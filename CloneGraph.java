820f249ce0e298a72e2687e292b4b11ff101a7f3
Guanda Wang <guanda.wang@gmail.com>
2017-01-12 22:41:05 -0500

    Clear indents

--- CloneGraph.java
+++ CloneGraph.java
@@ -1,7 +1,6 @@
 /*
 Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
 
-
 OJ's undirected graph serialization:
 Nodes are labeled uniquely.
 
@@ -14,16 +13,13 @@ First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
 Second node is labeled as 1. Connect node 1 to node 2.
 Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
 Visually, the graph looks like the following:
-
        1
       / \
      /   \
     0 --- 2
          / \
          \_/
-
 */
-
 /**
 * Definition for undirected graph.
 * class UndirectedGraphNode {
