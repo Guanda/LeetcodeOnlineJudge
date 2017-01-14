4857c30894c6b1a9de3edbcc20f1bae4ffd717fe
Guanda Wang <guanda.wang@gmail.com>
2017-01-13 23:30:10 -0500

    Update  code, make it cleaner

--- CountSmallerNumbersAfterSelf.java
+++ CountSmallerNumbersAfterSelf.java
@@ -21,17 +21,23 @@ class CountSmallerNumbersAfterSelf {
 	public List<Integer> countSmaller(int[] nums) {
 	    Integer[] ans = new Integer[nums.length];
 	    List<Integer> sorted = new ArrayList<Integer>();
+
 	    for (int i = nums.length - 1; i >= 0; i--) {
 	        int index = findIndex(sorted, nums[i]);
 	        ans[i] = index;
 	        sorted.add(index, nums[i]);
 	    }
+
 	    return Arrays.asList(ans);
 	}
+
 	private int findIndex(List<Integer> sorted, int target) {
-	    if (sorted.size() == 0) return 0;
+	    if (sorted.size() == 0) 
+	    	return 0;
+
 	    int start = 0;
 	    int end = sorted.size() - 1;
+
 	    if (sorted.get(end) < target) 
 	    	return end + 1;
 	    if (sorted.get(start) >= target) 
@@ -40,14 +46,18 @@ class CountSmallerNumbersAfterSelf {
 	    //binary search
 	    while (start + 1 < end) {
 	        int mid = start + (end - start) / 2;
+
 	        if (sorted.get(mid) < target) {
 	            start = mid + 1;
-	        } else {
+	        } 
+	        else {
 	            end = mid;
 	        }
 	    }
+
 	    if (sorted.get(start) >= target) 
 	    	return start;
+	    
 	    return end;
 	}
 }
