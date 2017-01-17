31505ffee34eb38020a6847821795496967fd425
Guanda Wang <guanda.wang@gmail.com>
2017-01-17 10:10:17 -0800

    updates

--- ValidSudoku.java
+++ ValidSudoku.java
@@ -1,7 +1,5 @@
 /*
-
 Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
-
 The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
 
 A partially filled sudoku which is valid.
@@ -11,8 +9,8 @@ A valid Sudoku board (partially filled) is not necessarily solvable. Only the fi
 be validated.
 
 Analysis:
-The number in each row, col and block should be unique. Go through every position of given board, 
-check if the number has been found in current row, column or block. If so, return false.
+	The number in each row, col and block should be unique. Go through every position of given board, 
+	check if the number has been found in current row, column or block. If so, return false.
 */
 
 class ValidSudoku
@@ -27,32 +25,26 @@ class ValidSudoku
 		ArrayList<boolean[]> colChecker = new ArrayList<boolean[]>();
 		ArrayList<boolean[]> blockChecker = new ArrayList<boolean[]>();
 
-		for(int i = 0; i < 9; i++)
-		{
+		for(int i = 0; i < 9; i++){
 			rowChecker.add(new boolean[9]);
 			colChecker.add(new boolean[9]);
 			blockChecker.add(new boolean[9]);
 		}
 
 		//go through every position
-		for(int i = 0; i < 9; i++)
-		{
-			for(int j = 0; j < 9; j++)
-			{
-				if(board[i][j] == '.')
-				{
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				if(board[i][j] == '.'){
 					continue;
 				}
 
 				int c = board[i][j] - '1';
 				if(rowChecker.get(j)[c] == true || 
 					colChecker.get(i)[c] == true || 
-					blockChecker.get(i/3*3 + j/3)[c] == true)
-				{
+					blockChecker.get(i/3*3 + j/3)[c] == true){
 					return false;
 				}
-				else
-				{
+				else{
 					rowChecker.get(j)[c] = true;
 					colChecker.get(i)[c] = true;
 					blockChecker.get(i/3*3 + j/3)[c] = true;
