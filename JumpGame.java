2f34159226338398363a079c3f553bb469ddcf5d
Guanda Wang <guanda.wang@gmail.com>
2017-02-22 22:59:19 -0500

    Jump game updates

--- JumpGame.java
+++ JumpGame.java
@@ -1,48 +1,40 @@
 /*
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
-
 Each element in the array represents your maximum jump length at that position.
-
 Determine if you are able to reach the last index.
 
 For example:
 A = [2,3,1,1,4], return true.
-
 A = [3,2,1,0,4], return false.
 
 Analysis:
-Method 1:
-	Notice that the value of element i represents the maximum jump length.
-	Suppose we are at A[i].
-	If the value is no less than the distance to the last element, then yes;
-	Or if any of the elements within the jump range can reach the last one, then yes;
-	Otherwise, no.
+	Method 1:
+		Notice that the value of element i represents the maximum jump length.
+		Suppose we are at A[i].
+		If the value is no less than the distance to the last element, then yes;
+		Or if any of the elements within the jump range can reach the last one, then yes;
+		Otherwise, no.
 
-Method 2:
-	Actually, all we want to know whether the next can-reach-end element is within my 
-	range. So, during the iteration, we can store the position of the next can-reach-end 
-	element and then for each successor element, we only need to check whether that node 
-	is reachable.
-
+	Method 2:
+		Actually, all we want to know whether the next can-reach-end element is within my 
+		range. So, during the iteration, we can store the position of the next can-reach-end 
+		element and then for each successor element, we only need to check whether that node 
+		is reachable.
 */
 
-class JumpGame
-{
+class JumpGamev{
 	//Method 1: time O(n^2), time limit exceeded in OJ
-	public boolean canJump(int[] nums)
-	{
-		//take care of the edge case
+	public boolean canJump(int[] nums) {
 		if(nums.length <= 1)
 			return true;
 
 		boolean[] canJump = new boolean[nums.length];
-		for(int i = nums.length - 2, dist = 1; i >= 0; i--, dist++)
-		{
+		for(int i = nums.length - 2, dist = 1; i >= 0; i--, dist++) {
 			if(nums[i] >= dist)
 				canJump[i] = true;
-			else
-			{
+			else {
 				int j = 1;
+				//any of the elements within the jump range can reach the last one
 				while(j <= nums[i] && !canJump[i+j])
 					j++;
 				if(j <= nums[i])
@@ -52,19 +44,15 @@ class JumpGame
 		return canJump[0];
 	}
 
-
 	//Method 2
-	public boolean canJump(int[] nums)
-	{
+	public boolean canJump(int[] nums) {
 		if(nums.length <= 1)
 			return true;
 
 		int next = nums.length - 1;
-		for(int i = nums.length - 2; i >= 0; i--)
-		{
+		for(int i = nums.length - 2; i >= 0; i--) {
 			//check the distance between the next and current pointer
-			if(nums[i] >= (next - i))
-			{
+			if(nums[i] >= (next - i)) {
 				next = i;
 			}
 		}
--- JumpGame2.java
+++ JumpGame2.java
@@ -7,13 +7,13 @@ Your goal is to reach the last index in the minimum number of jumps.
 
 For example:
 Given array A = [2,3,1,1,4]
-
-The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+The minimum number of jumps to reach the last index is 2. 
+(Jump 1 step from index 0 to 1, then 3 steps to the last index.)
 
 Analysis
 Method 1:
-	we start from an O(n^2) algorithm where we go backwards, compute the minimum steps for the current element by checking 
-	each element within its range to find the next one with smallest number of steps.
+	we start from an O(n^2) algorithm where we go backwards, compute the minimum steps for the current element 
+	by checking each element within its range to find the next one with smallest number of steps.
 
 Method 2:
 	We can use Greedy algorithm here. The basic idea is each time when we jump, we jump to the farthest slot.
@@ -22,29 +22,22 @@ Method 2:
 	~ max is the farthest position we can reach so far, so every time when we jump, we set next to current max.
 	Note: It is possible that the endpoint is unreachable (e.g. 3 2 1 0 0 0 0 0 0 1). In that case, at some jump point, 
 	we will find out that we are jumping to the same point. Then, game over.
-
 */
 
-class JumpGame2
-{
+class JumpGame2 {
 	//Method 1: time limit exceeded
-	public int jump(int[] nums)
-	{
+	public int jump(int[] nums) {
 		if(nums.length <= 1)
 			return 0;
 
 		int[] steps = new int[nums.length - 1];
-		for(int i = nums.length - 2; i >= 0; i--)
-		{
-			if(nums[i] >= (nums.length - 1 - i))
-			{
+		for(int i = nums.length - 2; i >= 0; i--) {
+			if(nums[i] >= (nums.length - 1 - i)) {
 				steps[i] = 1;
 			}
-			else
-			{
+			else {
 				int min = nums.length;
-				for(int j = 1; j <= nums[i]; j++)
-				{
+				for(int j = 1; j <= nums[i]; j++) {
 					min = Math.min(min, steps[i+j]);
 				}
 				steps[i] = min + 1;
@@ -53,16 +46,13 @@ class JumpGame2
 		return steps[0];
 	}
 
-
 	//Method 2
-	public int jump(int[] nums)
-	{
+	public int jump(int[] nums) {
 		int steps = 0;
-		for(int i = 0, max = 0, next = 0; i < nums.length - 1 && next < nums.length - 1; i++)
-		{
+		for(int i = 0, max = 0, next = 0; i < nums.length - 1 && next < nums.length - 1; i++) {
 			max = Math.max(max, i+nums[i]);
-			if(i == next) //ready to jump
-			{
+			//ready to jump
+			if(i == next) {
 				//unreachable
 				if(max == next)
 					return -1;
