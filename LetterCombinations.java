a606562bbcbe2f8bedad504391c2fac1063a1173
Guanda Wang <guanda.wang@gmail.com>
2017-03-05 22:03:04 -0500

    no message

--- LetterCombinations.java
+++ LetterCombinations.java
@@ -6,19 +6,15 @@ A mapping of digit to letters (just like on the telephone buttons).
 Input:Digit string "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 
-
 Analysis:
 1. Use recursive method to solve this problem.
 2. Normal iteration.
-
 */
 
-public class LetterCombinations
-{
+public class LetterCombinations {
 	private HashMap<Character, String> map = new HashMap<Character, String>();
 
-	private void setup()
-	{
+	private void setup() {
 		map.put('0', "");
 		map.put('1', "");
 		map.put('2', "abc");
@@ -32,27 +28,22 @@ public class LetterCombinations
 	}
 
 	//Method 1: recursion
-	public ArrayList<String> letterCombinations(String digits)
-	{
+	public ArrayList<String> letterCombinations(String digits) {
 		setup();
 		ArrayList<String> result = new ArrayList<String>();
 		recursion(digits, 0, new String(), result);
 		return result;
 	}
 
-	private void recursion(String digits, int start, String res, ArrayList<String> result)
-	{
+	private void recursion(String digits, int start, String res, ArrayList<String> result) {
 		if(res.length() > digits.length())
 			return;
 		else if(res.length() == digits.length())
 			result.add(res);
-		else
-		{
-			for(int i = start; i < digits.length(); i++)
-			{
+		else {
+			for(int i = start; i < digits.length(); i++) {
 				String letters = map.get(digits.charAt(i));
-				for(int j = 0; j < letters.length(); j++)
-				{
+				for(int j = 0; j < letters.length(); j++) {
 					recursion(digits, i + 1, res + letters.charAt(j), result);
 				}
 			}
@@ -61,17 +52,14 @@ public class LetterCombinations
 
 
 	//Method 2:
-    public ArrayList<String> letterCombinations(String digits) 
-    {
+    public ArrayList<String> letterCombinations(String digits) {
 	    setup();
 	    ArrayList<String> res = new ArrayList<String>();
 	    ArrayList<String> preres = new ArrayList<String>();
 	    res.add("");
 
-	    for(int i=0;i<digits.length();i++)
-	    {
-	        for(String str: res)
-	        {
+	    for(int i=0;i<digits.length();i++) {
+	        for(String str: res) {
 	            String letters = map.get(digits.charAt(i));
 	            for(int j=0;j<letters.length();j++)
 	                preres.add(str+letters.charAt(j));
