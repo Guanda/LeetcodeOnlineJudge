eb69f6ec5fa69c6986d24149edd6998130a6c9ce
Guanda Wang <guanda.wang@gmail.com>
2017-03-12 22:05:37 -0400

    update analysis

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,5 +1,6 @@
 /*
-Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes 
+that can be built with those letters.
 
 This is case sensitive, for example "Aa" is not considered a palindrome here.
 
@@ -7,15 +8,16 @@ Note:
 Assume the length of given string will not exceed 1,010.
 
 Example:
-
 Input:
 "abccccdd"
-
 Output:
 7
-
 Explanation:
 One longest palindrome that can be built is "dccaccd", whose length is 7
+
+Analysis:
+    Count the number of same pairs, then this can be used to put in the different direction to consist 
+    of palindrome. Then if there exist more chars, we can put one in the middle.
 */
 class LongestPalindrome {
 	public int longestPalindrome(String s) {
