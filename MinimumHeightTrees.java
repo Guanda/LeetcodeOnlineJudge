3e480c361e0dd71408bc4066e727a993ab5ed4c4
Guanda Wang <guanda.wang@gmail.com>
2017-04-03 22:50:21 -0400

    no message

--- MinimumHeightTrees.java
+++ MinimumHeightTrees.java
@@ -32,28 +32,26 @@ Given n = 6, edges = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]
 return [3, 4]
 
 Analysis:
+	Our problem want us to find the minimum height trees and return their root labels. First we can think about 
+	a simple case -- a path graph.
+	For a path graph of n nodes, find the minimum height trees is trivial. Just designate the middle point(s) as roots.
+	Despite its triviality, let design a algorithm to find them.
 
-Our problem want us to find the minimum height trees and return their root labels. First we can think about 
-a simple case -- a path graph.
-For a path graph of n nodes, find the minimum height trees is trivial. Just designate the middle point(s) as roots.
-Despite its triviality, let design a algorithm to find them.
+	Suppose we don't know n, nor do we have random access of the nodes. We have to traversal. It is very easy to get 
+	the idea of two pointers. One from each end and move at the same speed. When they meet or they are one step away, 
+	(depends on the parity of n), we have the roots we want.
 
-Suppose we don't know n, nor do we have random access of the nodes. We have to traversal. It is very easy to get 
-the idea of two pointers. One from each end and move at the same speed. When they meet or they are one step away, 
-(depends on the parity of n), we have the roots we want.
+	For a tree we can do some thing similar. We start from every end, by end we mean vertex of degree 1 (aka leaves). 
+	We let the pointers move the same speed. When two pointers meet, we keep only one of them, until the last two 
+	pointers meet or one step away we then find the roots.
 
-For a tree we can do some thing similar. We start from every end, by end we mean vertex of degree 1 (aka leaves). 
-We let the pointers move the same speed. When two pointers meet, we keep only one of them, until the last two 
-pointers meet or one step away we then find the roots.
+	It is easy to see that the last two pointers are from the two ends of the longest path in the graph.
 
-It is easy to see that the last two pointers are from the two ends of the longest path in the graph.
+	The actual implementation is similar to the BFS topological sort. Remove the leaves, update the degrees of 
+	inner vertexes. Then remove the new leaves. Doing so level by level until there are 2 or 1 nodes left. What's 
+	left is our answer!
 
-The actual implementation is similar to the BFS topological sort. Remove the leaves, update the degrees of 
-inner vertexes. Then remove the new leaves. Doing so level by level until there are 2 or 1 nodes left. What's 
-left is our answer!
-
-The time complexity and space complexity are both O(n).
-
+	The time complexity and space complexity are both O(n).
 */
 class MinimumHeightTrees {
 	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
