b782107cf2ffdc8c95a90fd3c4cf60d72ad490f1
Guanda Wang <guanda.wang@gmail.com>
2017-04-11 22:49:53 -0400

    Min stack updates

--- MinStack.java
+++ MinStack.java
@@ -5,37 +5,71 @@ push(x) -- Push element x onto stack.
 pop() -- Removes the element on top of the stack.
 top() -- Get the top element.
 getMin() -- Retrieve the minimum element in the stack.
-
 */
 
-import java.util.Stack;
-
-class MinStack
-{
+//Method 1: use java stack
+class MinStack {
 	private Stack<Integer> stack = new Stack<Integer>();
 	private Stack<Integer> minStack = new Stack<Integer>();
 
-	public void push(int x)
-	{
+	public void push(int x) {
 		if(minStack.isEmpty() || x <= minStack.peek())
 			minStack.push(x);
 		stack.push(x);
 	}
 
-	public void pop()
-	{
+	public void pop() {
 		if(stack.peek().equals(minStack.peek()))
 			minStack.pop();
 		stack.pop();
 	}
 
-	public int top()
-	{
+	public int top() {
 		return stack.peek();
 	}
 
-	public int getMin()
-	{
+	public int getMin() {
 		return minStack.peek();
 	}
+}
+
+
+//Method 2: 不用内置Stack的实现
+class MinStack {
+    private Node head;
+    
+    public void push(int x) {
+        if(head == null) 
+            head = new Node(x, x);
+        else 
+            head = new Node(x, Math.min(x, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+    
+    private class Node {
+        int val;
+        int min;
+        Node next;
+        
+        private Node(int val, int min) {
+            this(val, min, null);
+        }
+        
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
 }
