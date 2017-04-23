6fe1577040a0544c050ce270be7777a0208cf47c
Guanda Wang <guanda.wang@gmail.com>
2017-04-23 13:45:26 -0400

    no message

--- StrStr.java
+++ StrStr.java
@@ -5,22 +5,21 @@ or -1 if needle is not part of haystack.
 
 (Check if a string existing in another string)
 
+Analysis:
+	Time complexity O(m*n), because substring
+	(better solution: Rabin-Karp)
 */
 
-class StrStr()
-{
-	public int strStr(String haystack, String needle)
-	{
+class StrStr() {
+	public int strStr(String haystack, String needle) {
 		if(haystack == null || needle == null)
 			return -1;
 
 		int start = 0;
 		int len = needle.length();
 
-		while(start + len <= haystack.length())
-		{
-			if(haystack.substring(start, start + len).equals(needle))
-			{
+		while(start + len <= haystack.length()) {
+			if(haystack.substring(start, start + len).equals(needle)) {
 				return start;
 			}
 			start++;
