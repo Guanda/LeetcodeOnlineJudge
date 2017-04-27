6b4ea69e94978b5b9d2a37f8bfd595e925eb8d8b
Guanda Wang <guanda.wang@gmail.com>
2017-04-26 22:58:05 -0400

    Add Rabin-Karp algorithem solution for strStr problem

--- StrStr.java
+++ StrStr.java
@@ -11,6 +11,7 @@ Analysis:
 */
 
 class StrStr() {
+	//Method 1: native
 	public int strStr(String haystack, String needle) {
 		if(haystack == null || needle == null)
 			return -1;
@@ -23,6 +24,60 @@ class StrStr() {
 				return start;
 			}
 			start++;
+		}
+		return -1;
+	}
+
+
+	//Method 2: Rabin-Karp
+	public int BASE = 1000000; //hash function base size, for mod purpose
+	public int strStr(String source, String target) {
+		if(source == null || target == null) {
+			return -1;
+		}
+
+		int len = target.length();
+		if(len == 0) {
+			return 0;
+		}
+
+		//31^len, 为后面补上一个char之后减去前面的char
+		int power = 1;
+		for(int i = 0; i < len; i++) {
+			power = (power * 31) % BASE;
+		}
+
+		int targetCode = 0;
+		for(int i = 0; i < len; i++) {
+			targetCode = (targetCode * 31 + target.charAt(i)) % BASE;
+		}
+
+		int hashCode = 0;
+		for(int i = 0; i < source.length(); i++) {
+			//abc + d
+			hashCode = (hashCode * 31 + source.charAt(i)) % BASE;
+
+			//如果不够target长度的字符，继续计算，不需要减去前面的字符
+			if(i < len - 1) {
+				continue;
+			}
+
+			//i = len 的情况不需要加减, 但仍然需要最后的double check步骤， 所以不能直接continue
+
+			//abcd - a
+			if(i >= len) {
+				hashCode = (hashCode - source.charAt(i-len) * power) % BASE;
+				if(hashCode < 0) {
+					hashCode += BASE;
+				}
+			}
+
+			//double check the string
+			if(hashCode == targetCode) {
+				if(source.substring(i-len+1, i+1).equals(target)) {
+					return i - len + 1;
+				}
+			}
 		}
 		return -1;
 	}
