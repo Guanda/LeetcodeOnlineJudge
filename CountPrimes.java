a48a63be788d20c40a0cd567de9a270295c18411
Guanda Wang <guanda.wang@gmail.com>
2017-06-09 23:38:27 -0400

    no message

--- CountPrimes.java
+++ CountPrimes.java
@@ -4,30 +4,23 @@ Description:
 Count the number of prime numbers less than a non-negative number, n
 
 Analysis:
-Sieve of Eratosthenes
-Remove all non-prime integers and then count the rest.
-
+	Sieve of Eratosthenes
+	Remove all non-prime integers and then count the rest.
 */
 
-class CountPrimes
-{
-	public int countPrimes(int n)
-	{
+class CountPrimes {
+	public int countPrimes(int n) {
 		boolean[] primes = new boolean[n];
-		for(int i = 2; i < n; i++)
-		{
+		for(int i = 2; i < n; i++) {
 			primes[i] = true;
 		}
 
 		//sieve of eratorsthenes
-		for(int i = 2; i*i < n; i++)
-		{
-			if(primes[i])
-			{
+		for(int i = 2; i*i < n; i++) {
+			if(primes[i]) {
 				//start from i*i, because all the multiple less than i already been implemented.
 				int c = i * i;
-				while(c < n)
-				{
+				while(c < n) {
 					primes[c] = false;
 					c = c + i;
 				}
@@ -35,8 +28,7 @@ class CountPrimes
 		}
 
 		int count = 0;
-		for(int i = 2; i < n; i++)
-		{
+		for(int i = 2; i < n; i++) {
 			if(primes[i])
 				count++;
 		}
--- SpiralMatrix2.java
+++ SpiralMatrix2.java
@@ -17,44 +17,36 @@ Method 1:
 
 Method 2:
 	Level by level.
-
 */
 
-class SpiralMatrix2
-{
+class SpiralMatrix2 {
 	//Method 1
-	public int[][] generateMatrix(int n)
-	{
+	public int[][] generateMatrix(int n) {
 		int val = 1;
 		int[][] matrix = new int[n][n];
 		int top = 0, bottom = n-1, left = 0, right = n-1;
 
-		while(left < right && top < bottom)
-		{
+		while(left < right && top < bottom) {
 			//top
-			for(int i = left; i < right; i++)
-			{
+			for(int i = left; i < right; i++) {
 				matrix[top][i] = val;
 				val++;
 			}
 
 			//right
-			for(int i = top; i < bottom; i++)
-			{
+			for(int i = top; i < bottom; i++) {
 				matrix[i][right] = val;
 				val++;
 			}
 
 			//bottom
-			for(int i = right; i > left; i--)
-			{
+			for(int i = right; i > left; i--) {
 				matrix[bottom][i] = val;
 				val++;
 			}
 
 			//left
-			for(int i = bottom; i > top; i--)
-			{
+			for(int i = bottom; i > top; i--) {
 				matrix[i][left] = val;
 				val++;
 			}
@@ -73,40 +65,34 @@ class SpiralMatrix2
 
 
 	//Method 2
-	public int[][] generateMatrix(int n)
-	{
+	public int[][] generateMatrix(int n) {
 		int val = 1;
 		int[][] matrix = new int[n][n];
 
-		for(int level = 0; level < n; level++, n--)
-		{
+		for(int level = 0; level < n; level++, n--) {
 			//top
-			for(int i = level; i < n; i++)
-			{
+			for(int i = level; i < n; i++) {
 				matrix[level][i] = val;
 				val++;
 			}
 
 			//right
-			for(int i = level+1; i < n; i++)
-			{
+			for(int i = level+1; i < n; i++) {
 				matrix[i][n-1] = val;
 				val++;
 			}
 
 			//bottom
-			for(int i = n-2; i >= level; i--)
-			{
+			for(int i = n-2; i >= level; i--) {
 				matrix[n-1][i] = val;
 				val++;
 			}
 
 			//left
-			for(int i = n-2; i > level; i--)
-			{
+			for(int i = n-2; i > level; i--) {
 				matrix[i][level] = val;
 				val++;
-			}
+			}	
 		}
 		return matrix;
 	}
