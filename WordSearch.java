ec2b9142918b4ca13a0908fc94913aafb23271b5
Guanda Wang <guanda.wang@gmail.com>
2017-06-19 23:00:02 -0400

    Review the code for Word Search problem

--- WordSearch.java
+++ WordSearch.java
@@ -18,64 +18,64 @@ word = "SEE", -> returns true,
 word = "ABCB", -> returns false.
 
 Analysis:
-Consider the board as a graph where edges connect adjacent cells. Now the problem becomes 
-finding a path in a graph. Given a starting node, we can use DFS or BFS to find a path. 
-But for this problem, we have to find the starting node first.
+	Consider the board as a graph where edges connect adjacent cells. Now the problem becomes 
+	finding a path in a graph. Given a starting node, we can use DFS or BFS to find a path. 
+	But for this problem, we have to find the starting node first.
 
-The basic idea is:
+	The basic idea is:
+		Loop through the board find out every cell which start with word's first character and then
+		start dfs to match the word. The way we track the visited cells is mark the visited cell to
+		'#'.
 
-	Loop through every cell in the board and perform DFS to match the word. Since a same cell 
-	cannot be reused, we also need to keep track of visited cells during DFS and unmark it if 
-	it is not being used.
-
-We can also use BFS, but it requires to store the intermediate results, which in this problem 
-is a tuple (x, y). Creating objects for tuples is quite expensive.
-
+	We can also use BFS, but it requires to store the intermediate results, which in this problem 
+	is a tuple (x, y). Creating objects for tuples is quite expensive.
 */
 
-class WordSearch
-{
-	public boolean exist(char[][] board, String word)
-	{
-		boolean[][] visited = new boolean[board.length][board[0].length];
-
-		//find the starting char
-		for(int i = 0; i < board.length; i++)
-		{
-			for(int j = 0; j < board[0].length; j++)
-			{
-				if(DFS(board, i, j, word, 0, visited))
-					return true;
-			}
-		}
-		return false;
-	}
-
-	public boolean DFS(char[][] board, int x, int y, String word, int curr, boolean[][] visited)
-	{
-		//validate input
-		if(visited[x][y] || board[x][y] != word.charAt(curr))
-			return false;
-
-		//stop case
-		if(curr == word.length() - 1)
-			return true;
-
-		//mark the node as visited
-		visited[x][y] = true;
-
-		//DFS on its neighbers
-		if(x > 0 && DFS(board, x-1, y, word, curr+1, visited))
-			return true;
-		if(x+1 < board.length && DFS(board, x+1, y, word, curr+1, visited))
-			return true;
-		if(y > 0 && DFS(board, x, y-1, word, curr+1, visited))
-			return true;
-		if(y+1 < board[0].length && DFS(board, x, y+1, word, curr+1, visited))
-			return true;
-
-		//mark the node unused
-		visited[x][y] = false;
-		return false;
-	}
+class WordSearch {
+    /**
+     * @param board: A list of lists of character
+     * @param word: A string
+     * @return: A boolean
+     */
+    public boolean exist(char[][] board, String word) {
+        if(board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        
+        if(word.length() == 0) {
+            return true;
+        }
+        
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == word.charAt(0)) {
+                    boolean result = dfs(board, i, j, word, 0);
+                    if(result) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, int i, int j, String word, int start) {
+        if(start == word.length()) {
+            return true;
+        }
+        
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length 
+        || board[i][j] != word.charAt(start)) {
+            return false;
+        }
+        
+        //mark the visited element
+        board[i][j] = '#';
+        boolean result = dfs(board, i, j+1, word, start + 1) || 
+                        dfs(board, i, j-1, word, start + 1) ||
+                        dfs(board, i+1, j, word, start + 1) ||
+                        dfs(board, i-1, j, word, start + 1);
+        board[i][j] = word.charAt(start);
+        return result;
+    }
 }
