cfa931ad0996173a46549a9642aa2326480a74ab
Guanda Wang <guanda.wang@gmail.com>
2017-07-03 13:49:06 -0400

    Trapping rain water better solution

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -7,13 +7,15 @@ For example,
 Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 
 Analysis:
+	Method 1:
+		对任意位置i，在i上的积水，由左右两边最高的bar：A[left] = max{A[j], j<i}, A[right] = max{A[j], j>i}决定。
+		定义Hmin = min(A[left], A[right])，则积水量Si为：
+		Hmin <= A[i]时，Si = 0
+		Hmin > A[i]时，Si = Hmin - A[i]
 
-对任意位置i，在i上的积水，由左右两边最高的bar：A[left] = max{A[j], j<i}, A[right] = max{A[j], j>i}决定。
-定义Hmin = min(A[left], A[right])，则积水量Si为：
-
-Hmin <= A[i]时，Si = 0
-Hmin > A[i]时，Si = Hmin - A[i]
-
+	Method 2:
+		先看最左最右两根，确定灌水基调，从小的那边灌起。
+		从两端往中间灌
 */
 
 class TrappingRainWater {
@@ -45,31 +47,37 @@ class TrappingRainWater {
 		return water;
 	}
 
-	//optimize of method 1
+	//Method 2:
 	public int trapBetter(int[] height) {
-		if(height == null || height.length == 0) {
-			return 0;
-		}
-
-		int water = 0;
-		int left = 0;
-		int right = 0;
-		int start = 0;
-		int end = height.length - 1;
-
-		while(start < end) {
-			left = Math.max(left, height[start]);
-			right = Math.max(right, height[end]);
-
-			if(left < right) {
-				water += left - height[start];
-				start++;
-			}
-			else {
-				water += right - height[end];
-				end--;
-			}
-		}
-		return water;
+        if(heights == null || heights.length <= 1) {
+            return 0;
+        }
+        
+        int left = 0, right = heights.length - 1;
+        int leftHeight = heights[left];
+        int rightHeight = heights[right];
+        int result = 0;
+        
+        while(left < right) {
+            if(leftHeight < rightHeight) {
+                left++;
+                if(leftHeight > heights[left]) {
+                    result += leftHeight - heights[left];
+                }
+                else {
+                    leftHeight = heights[left];
+                }
+            }
+            else {
+                right--;
+                if(rightHeight > heights[right]) {
+                    result += rightHeight - heights[right];
+                }
+                else {
+                    rightHeight = heights[right];
+                }
+            }
+        }
+        return result;
 	}
 }
