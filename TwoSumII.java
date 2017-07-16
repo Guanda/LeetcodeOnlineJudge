4e9da988a0f6998be4921c40e7a158eb60758fc2
Guanda Wang <guanda.wang@gmail.com>
2017-07-16 15:56:19 -0400

    no message

--- TwoSumII.java
+++ TwoSumII.java
@@ -12,33 +12,26 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 
 Analysis:
-Similar with the 2Sum problem, using two pointers from both sides
-
+	Similar with the 2Sum problem, using two pointers from both sides
 */
 
-class TwoSumII
-{
-	public int[] twoSum(int[] nums, int target)
-	{
+class TwoSumII {
+	public int[] twoSum(int[] nums, int target) {
 		if(nums == null || nums.length == 0)
 			return null;
 
 		int[] result = new int[2];
 		int i = 0;
 		int j = nums.length - 1;
-		while(i < j)
-		{
+		while(i < j) {
 			int tmp = nums[i] + nums[j];
-			if(tmp < target)
-			{
+			if(tmp < target) {
 				i++;
 			}
-			else if(tmp > target)
-			{
+			else if(tmp > target) {
 				j--;
 			}
-			else
-			{
+			else {
 				//the result is not zero-based, so plus 1.
 				result[0] = i+1;
 				result[1] = j+1;
