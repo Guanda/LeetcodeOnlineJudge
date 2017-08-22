d29e193062c720aae6c132c9b341f2c9a3cb3ad7
Guanda Wang <guanda.wang@gmail.com>
2017-08-21 20:20:14 -0400

    update for Valid Parentheses problem

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -2,38 +2,36 @@
 Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
-
 */
 
-class ValidParentheses
-{
-	public boolean isValid(String s)
-	{
-		Stack<Character> stack = new Stack<>();
-		for(int i = 0; i < s.length(); i++)
-		{
-			char ch = s.charAt(i);
-			if(ch == '(' || ch == '{' || ch == '[')
-				stack.push(ch);
-			if(ch == ')')
-			{
-				if(stack.isEmpty() || stack.peek() != '(')
-					return false;
-				stack.pop();
-			}
-			if(ch == '}')
-			{
-				if(stack.isEmpty() || stack.peek() != '{')
-					return false;
-				stack.pop();
-			}
-			if(ch == ']')
-			{
-				if(stack.isEmpty() || stack.peek() != '[')
-					return false;
-				stack.pop();
-			}
-		}
-		return stack.isEmpty();
+class ValidParentheses {
+	public boolean isValid(String s) {
+        if(s == null || s.length() == 0) {
+            return false;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            else if(!stack.isEmpty()) {
+                if(c == ')' && stack.peek() != '(') {
+                    return false;
+                }
+                else if(c == ']' && stack.peek() != '[') {
+                    return false;
+                }
+                else if(c == '}' && stack.peek() != '{') {
+                    return false;
+                }
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
 	}
 }
