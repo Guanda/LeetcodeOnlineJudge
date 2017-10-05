6b80a18b10ca1ac951ef2100dcc5eb9379d2a6b0
Guanda Wang <guanda.wang@gmail.com>
2017-10-04 20:16:23 -0400

    Combination Sum problem, add DP solution

--- CombinationSum.java
+++ CombinationSum.java
@@ -43,4 +43,38 @@ public class CombinationSum {
 		}
 		return;
 	}
+
+
+	// DP solution
+	public List<List<Integer>> combinationSumDP(int[] candidates, int target) {
+
+		Arrays.sort(candidates); // sort candidates to try them in asc order
+
+		List<List<List<Integer>>> dp = new ArrayList<>();
+
+		for (int i = 1; i <= target; i++) { // run through all targets from 1 to t
+			List<List<Integer>> newList = new ArrayList<>(); // combs for curr i
+			// run through all candidates <= i
+			for (int j = 0; j < candidates.length && candidates[j] <= i; j++) {
+				// special case when curr target is equal to curr candidate
+				if (i == candidates[j]) {
+					newList.add(Arrays.asList(candidates[j]));
+				}
+				// if current candidate is less than the target use prev results
+				else {
+					for (List<Integer> l : dp.get(i - candidates[j] - 1)) {
+						// make sure it is in ascending order and no duplicates
+						if (candidates[j] <= l.get(0)) {
+							List cl = new ArrayList<>();
+							cl.add(candidates[j]);
+							cl.addAll(l);
+							newList.add(cl);
+						}
+					}
+				}
+			}
+			dp.add(newList);
+		}
+		return dp.get(target - 1);
+	}
 }
