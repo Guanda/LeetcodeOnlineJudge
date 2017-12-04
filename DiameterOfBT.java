5bb48812d3c7493b73c773a5b85adb4a56af875b
Guanda Wang <guanda.wang@gmail.com>
2017-12-04 15:51:12 -0800

    updates

--- DiameterOfBT.java
+++ DiameterOfBT.java
@@ -17,21 +17,21 @@ class DiameterOfBT {
 
 	int max = 0;
 
-    public int diameterOfBinaryTree(TreeNode root) {
-        maxDepth(root);
-        return max;
-    }
+  public int diameterOfBinaryTree(TreeNode root) {
+      maxDepth(root);
+      return max;
+  }
 
-    private int maxDepth(TreeNode root) {
-    	if(root == null) {
-    		return 0;
-    	}
+  private int maxDepth(TreeNode root) {
+  	if(root == null) {
+  		return 0;
+  	}
 
-    	int left = maxDepth(root.left);
-    	int right = maxDepth(root.right);
+  	int left = maxDepth(root.left);
+  	int right = maxDepth(root.right);
 
-    	max = Math.max(max, left + right);
+  	max = Math.max(max, left + right);
 
-    	return Math.max(left, right) + 1;
-    }
+  	return Math.max(left, right) + 1;
+  }
 }
