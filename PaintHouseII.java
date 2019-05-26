2a67119ce3306b93f3a4f14910ff2fcf194077a8
Guanda Wang <guanda.wang@gmail.com>
2019-05-26 14:09:55 -0700

    no message

--- PaintHouseII.java
+++ PaintHouseII.java
@@ -19,43 +19,40 @@ Analysis:
 	最小和第二小的花费的颜色，如果当前房子颜色和min1相同，那么我们用min2对应的值计算，反之我们用min1对应的值，
 	这种解法实际上也包含了求次小值的方法
 */
-class PaintHouseII {
-    /**
-     * @param costs n x k cost matrix
-     * @return an integer, the minimum cost to paint all houses
-     */
+class Solution {
     public int minCostII(int[][] costs) {
-        // Write your code here
-        if (costs == null || costs.length == 0) return 0;
-        
-        int n = costs.length, k = costs[0].length;
+        if(costs == null || costs.length == 0)
+            return 0;
+        int k = costs[0].length;
         // min1 is the index of the 1st-smallest cost till previous house
         // min2 is the index of the 2nd-smallest cost till previous house
         int min1 = -1, min2 = -1;
-        
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < costs.length; i++){
+            // use last1 and last2 to keep maintain mins
             int last1 = min1, last2 = min2;
-            min1 = -1; min2 = -1;
+            // then reset min1 and min2 for current house painting
+            min1 = -1;
+            min2 = -1;
             
-            for (int j = 0; j < k; j++) {
-                if (j != last1) {
+            for(int j = 0; j < k; j++){
+                if(j != last1){
                     // current color j is different to last min1
-                    costs[i][j] += last1 < 0 ? 0 : costs[i - 1][last1];
-                } 
+                    costs[i][j] += last1 < 0 ? 0 : costs[i-1][last1];
+                }
                 else {
-                    costs[i][j] += last2 < 0 ? 0 : costs[i - 1][last2];
+                    costs[i][j] += last2 < 0 ? 0 : costs[i-1][last2];
                 }
                 
                 // find the indices of 1st and 2nd smallest cost of painting current house i
-                if (min1 < 0 || costs[i][j] < costs[i][min1]) {
-                    min2 = min1; min1 = j;
-                } 
-                else if (min2 < 0 || costs[i][j] < costs[i][min2]) {
+                if(min1 < 0 || costs[i][j] < costs[i][min1]){
+                    min2 = min1;
+                    min1 = j;
+                }
+                else if(min2 < 0 || costs[i][j] < costs[i][min2]) {
                     min2 = j;
                 }
             }
         }
-        
-        return costs[n - 1][min1];
+        return costs[n-1][min1];
     }
 }
